c62d415bdc5e2bad5f103cb3c9d65e4d952a139a
zhengzuozhang <2570389155@qq.com>
2021-09-02 11:36:00 +0800

    adminserver

---
+++ com.lz.finance.adminserver/src/test/java/com/lz/finance/adminserver/AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz.finance.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
